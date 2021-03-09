7c97a60bff13cefbf54a8351989d8b197845c5cf
Thomas <tomad212@gmail.com>
2021-03-09 11:49:27 +0200

    Add docker-compose and update index.js

--- section-5/webserver/index.js
+++ section-5/webserver/index.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const redis = require('redis');
+const process = require('process');
 
 const app = express();
-const client = redis.createClient();
+const client = redis.createClient({
+    host: 'redis-server',
+    port: 6379
+});
 client.set('visits', 0);
 
 app.get('/', (req, res) => {
+    // purposfully added to crash container to test docker-compose restart
+    process.exit(0);
     console.log('Tracking Connections');
     client.get('visits', (err, visits) => {
         res.send(`Number of visits - ${visits}`);
